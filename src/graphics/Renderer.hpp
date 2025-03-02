a11bd9d5c5c50fef8f2f790cafedd2d67e0e9486
Kevin <ktu3@ualberta.ca>
2025-03-01 21:30:59 -0700

    Fix include slash characters to forward slash and change project name in CMake

--- src/graphics/Renderer.hpp
+++ src/graphics/Renderer.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <glad\glad.h>
+#include <glad/glad.h>
 #include "IndexBuffer.hpp"
 #include "VertexArray.hpp"
 #include "Shader.hpp"
--- src/graphics/VertexArray.hpp
+++ src/graphics/VertexArray.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <glad\glad.h>
+#include <glad/glad.h>
 #include "VertexBuffer.hpp"
 #include "IndexBuffer.hpp"
 
