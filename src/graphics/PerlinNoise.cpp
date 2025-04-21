522551500ce23373a84c0d067743721c32cca78b
Kevin Tu <tukevin274@gmail.com>
2025-04-20 19:31:29 -0600

    Finish SamplePerlin function

--- src/graphics/PerlinNoise.cpp
+++ src/graphics/PerlinNoise.cpp
@@ -7,6 +7,12 @@ glm::vec2 PerlinNoise::RandomGradient(int x, int y)
 	return retVector;
 }
 
+float PerlinNoise::PerlinInterpolate(float x, float y, float weight)
+{
+	// Utilizes cubic interpolation
+	return (y - x) * (3.0 - weight * 2.0) * weight * weight + x;
+}
+
 float PerlinNoise::SamplePerlin(float x, float y)
 {
 	// Determine corner coordinates of the grid
@@ -18,6 +24,18 @@ float PerlinNoise::SamplePerlin(float x, float y)
 	// Interpolation weights
 	float xWeight = x - (float)x0;
 	float yWeight = y - (float)y0;
+
+	// Calculate gradients and interpolation for top corners
+	float g_TopL = PerlinDot(x0, y0, x, y);
+	float g_TopR = PerlinDot(x1, y0, x, y);
+	float topInterp = PerlinInterpolate(g_TopL, g_TopR, xWeight);
+
+	// Calculate gradients and interpolation for bottom corners
+	float g_BotL = PerlinDot(x0, y1, x, y);
+	float g_BotR = PerlinDot(x1, y1, x, y);
+	float botInterp = PerlinInterpolate(g_BotL, g_BotR, xWeight);
+
+	float ret = PerlinInterpolate(topInterp, botInterp, yWeight);
 }
 
 float PerlinNoise::PerlinDot(int gridX, int gridY, float x, float y)
--- src/graphics/PerlinNoise.hpp
+++ src/graphics/PerlinNoise.hpp
@@ -1,9 +1,15 @@
 #pragma once
 #include <math.h>
+#include <glm/glm.hpp>
+
 
 class PerlinNoise
 {
+private:
+	glm::vec2 RandomGradient(int x, int y);
+	float PerlinInterpolate(float x, float y, float weight);
+	float PerlinDot(int gridX, int gridY, float x, float y);
+
 public:
 	float SamplePerlin(float x, float y);
-	float PerlinDot(int gridX, int gridY, float x, float y);
 };
