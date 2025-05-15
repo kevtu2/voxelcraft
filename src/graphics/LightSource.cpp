c51c1071fc842b268bba23482f44d0d3a6beaede
Kevin Tu <tukevin274@gmail.com>
2025-05-15 12:12:51 -0600

    Add LightSource class and fragment shader

---
+++ src/graphics/LightSource.cpp
@@ -0,0 +1,19 @@
+#include "LightSource.hpp"
+
+LightSource::LightSource()
+{
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+
+	glGenBuffers(1, &lightVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+}
+
+LightSource::~LightSource()
+{
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteBuffers(1, &lightVBO);
+}
---
+++ src/graphics/LightSource.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+class LightSource
+{
+public:
+	LightSource();
+
+private:
+	unsigned int lightVAO;
+};
