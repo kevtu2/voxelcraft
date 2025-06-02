6ff6d7bb04420e7cb69a275e0af4527e12eb3562
Kevin Tu <tukevin274@gmail.com>
2025-06-02 14:42:34 -0600

    Finish drawing function definitions for aabb box

--- src/graphics/AABB.cpp
+++ src/graphics/AABB.cpp
@@ -21,17 +21,34 @@ AABB::AABB(const glm::vec3& position, float height, float width) :
 #ifdef DEBUG
 void AABB::GenerateAABBMesh()
 {
-
+	glGenVertexArrays(1, &VAO_ID);
+	glGenBuffers(1, &VBO_ID);
+	glGenBuffers(1, &EBO_ID);
 }
 
 void AABB::BufferAABBData()
 {
+	glBindVertexArray(VAO_ID);
 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_ID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_ID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
 }
 
 void AABB::DrawArrays()
 {
+	glBindVertexArray(VAO_ID);
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glDrawElements(GL_TRIANGLES, 32, GL_UNSIGNED_INT, 0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 #endif
 
