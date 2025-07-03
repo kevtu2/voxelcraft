ab8f829087a786d3dfb82134ce22603794c48bd3
Kevin Tu <tukevin274@gmail.com>
2025-07-03 13:36:42 -0600

    Add singleton implementation for UIManager

--- src/UI/UIManager.hpp
+++ src/UI/UIManager.hpp
@@ -1,6 +1,14 @@
 #pragma once
+#include <memory>
 class UIManager
 {
+private:
+	static UIManager Manager;
+public:
+	static UIManager& GetInstance();
 
+	UIManager() = default;
+	UIManager(const UIManager& other) = delete;
+	void operator=(const UIManager&) = delete;
 };
 
