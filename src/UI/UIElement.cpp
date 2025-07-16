e22e3f38c1e587bf14618cc42eef7ab0164fecb7
Kevin Tu <tukevin274@gmail.com>
2025-07-16 15:36:11 -0600

    Test

--- src/UI/UIElement.cpp
+++ src/UI/UIElement.cpp
@@ -19,7 +19,7 @@ UIElement::UIElement(UIState& state) :
 
 void UIElement::CentreNextItem(float itemWidth)
 {
-	float avail = ImGui::GetWindowWidth();
+	float avail = ImGui::GetWindowWidth() / contentScale;
 	ImGui::SetCursorPosX((avail - itemWidth) * 0.5f);
 }
 
